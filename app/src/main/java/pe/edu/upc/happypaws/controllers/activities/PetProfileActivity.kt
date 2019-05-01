a250be29d0161396b5d4b5f74c9c2479d6a39974
Francesca Haro <francescaharo@Francescas-MacBook-Pro.local>
2019-05-01 14:57:39 -0500

    Profile Views

--- app/src/main/java/pe/edu/upc/happypaws/controllers/activities/PetProfileActivity.kt
+++ app/src/main/java/pe/edu/upc/happypaws/controllers/activities/PetProfileActivity.kt
@@ -1,20 +1,13 @@
 package pe.edu.upc.happypaws.controllers.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
 import pe.edu.upc.happypaws.R
 
-class LoginActivity : AppCompatActivity() {
+class PetProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet_profile)
-
-        login_btn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
