73a77cbcc1e8c04ca32cc07757c2bc2068c2b7d2
bryam <bryam.vasquez@tektonlabs.com>
2019-05-02 11:15:43 -0500

    api key

--- app/src/main/java/pe/edu/upc/happypaws/networking/HappyPawsApi.kt
+++ app/src/main/java/pe/edu/upc/happypaws/networking/HappyPawsApi.kt
@@ -5,7 +5,7 @@ import pe.edu.upc.happypaws.models.Pet
 class HappyPawsApi {
     companion object {
         val BASE_URL = "https://happypaws-api.herokuapp.com/api"
-        val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjp7ImlkIjoiNWNiNjNmYWJkNmQ0YmYwMDE3YThkYzk1IiwidGltZXN0YW1wIjoxNTU2Nzc5NTMwODk5fSwiaWF0IjoxNTU2Nzc5NTMwLCJleHAiOjE1NTY3ODEzMzB9.kX3svIq57IZCm1Pv0ymiGj-hj06wTCq11yeZNFVYOcQ"
+        val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uIjp7ImlkIjoiNWNiNjNmYWJkNmQ0YmYwMDE3YThkYzk1IiwidGltZXN0YW1wIjoxNTU2ODEzMzg4ODE0fSwiaWF0IjoxNTU2ODEzMzg4LCJleHAiOjE1NTY4MTUxODh9.rbPfbncNrlSAiPUPLGaMU3ubXHNtYlA43W7h6bwC3ZU"
 
         fun petsUrl(): String {
             return "${BASE_URL}/pets"
