7ee313fe215310f55ed2c123ff5b20829a18b276
caguilar <caguilar@belatrixsf.com>
2019-05-01 20:09:58 -0500

    bryam ptaso

--- app/src/main/java/pe/edu/upc/happypaws/controllers/activities/MainActivity.kt
+++ app/src/main/java/pe/edu/upc/happypaws/controllers/activities/MainActivity.kt
@@ -24,7 +24,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         navigateTo(navigation.menu.findItem(R.id.navigation_profile))
-
     }
 
     private fun getFragmentFor(item: MenuItem) : Fragment {
