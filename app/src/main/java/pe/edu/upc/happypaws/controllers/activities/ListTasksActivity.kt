beb66ae63952f1a983af30effdf4994640bdb691
FrancescaHarobtx <fharo@belatrixsf.com>
2019-05-30 11:10:29 -0500

    fixes

---
+++ app/src/main/java/pe/edu/upc/happypaws/controllers/activities/ListTasksActivity.kt
@@ -0,0 +1,13 @@
+package pe.edu.upc.happypaws.controllers.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pe.edu.upc.happypaws.R
+
+class ListTasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_tasks)
+    }
+}
